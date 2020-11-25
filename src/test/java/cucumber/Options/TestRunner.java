3fdf7c5b1aa8470fabd321141abbb2bab26a7b41
dhananjaya <64034014+Dhananjaya-bn@users.noreply.github.com>
2020-11-25 11:46:48 +0530

    small changes

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/java/features",plugin="json:target/jsonReports/cucumber-report.json",glue={"stepDefinations"})
+@CucumberOptions(features="src/test/java/features",plugin="json:target/jsonReports/cucumber-report.json",glue={"stepDefinations"},monochrome = true)
 public class TestRunner {
 
 }
